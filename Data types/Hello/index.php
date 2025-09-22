1c98904edc48bd7fd6303387524d1d8b93f87d0c
Alla <tuninaalla53@gmail.com>
2025-09-22 22:15:58 +0300

    Функции

---
+++ Data types/Hello/index.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+$name = "Alla"; 
+
+echo "Hello! My name is '$name'"; 
+?>
+
+     <!-- <a href="/Hello">Hello</a> -->
+</body>
+</html>
---
+++ Data types/Summa/index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$a = 5; 
+$b = 10; 
+
+
+$rez = $a + $b;
+
+
+echo "'$a' + '$b' = '$rez'"; 
+?>
+</body>
+</html>
